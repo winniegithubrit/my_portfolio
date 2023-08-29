d0b88e9446d43f38539dd37d0ae11ba377be49a9
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-08-29 18:35:40 +0300

    Updated the home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,10 +2,39 @@ import React from "react";
 import "../App.css";
 
 function Home() {
-  return (
-    <div className="Home">
-      HomePage
+  const containerStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center", 
+    background: "linear-gradient(45deg, pink, purple)",
+    padding: "50px",
+    color: "black",
+  };
 
+  const textStyle = {
+    fontSize: "60px",
+    fontWeight: "bold",
+    margin: "0",
+    flex: "1", 
+  };
+
+  const imageStyle = {
+    maxWidth: "100%",
+    height: "auto",
+    marginLeft: "20px",
+  };
+
+  return (
+    <div className="Home" style={containerStyle}>
+      <p style={textStyle}>
+        Hello there, My Name is Winnie Jomo <br />A Full Stack Software
+        Developer
+      </p>
+      <img
+        src="https://images.pexels.com/photos/3189024/pexels-photo-3189024.jpeg?auto=compress&cs=tinysrgb&w=400"
+        alt="Developer"
+        style={imageStyle}
+      />
     </div>
   );
 }
